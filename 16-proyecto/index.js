c8a7f031c7b93ddb1f3a353143cba5e063c592be
Cristian David Corrales Ospina <ccorrales@PASCUALBRAVO.CENTRAL>
2024-05-17 16:56:28 -0500

    Último avance del día JavaScript

---
+++ 16-proyecto/index.js
@@ -0,0 +1,22 @@
+class User {
+  static #url = "https://jsonplaceholder.typicode.com/users";
+  static #users = [];
+
+  static async getAll() {
+    try {
+      const response = await fetch(this.#url);
+      if (!response.ok) throw response;
+      this.#users = await response.json();
+      return this.#users;
+    } catch (error) {
+      console.log("Error:", error);
+    }
+  }
+}
+
+async function main() {
+  const users = await User.getAll();
+  console.log(users);
+}
+
+main();
